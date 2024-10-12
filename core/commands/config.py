509f020453d5b9dbe4a418c3182c5de242123af6
Jianliang Wang <125073156+jianliang00@users.noreply.github.com>
2024-10-12 03:12:05 +0000

    chore: update entity in copyright statement

--- core/commands/config.py
+++ core/commands/config.py
@@ -1,4 +1,4 @@
-# Copyright 2024 The habitat Authors. All rights reserved.
+# Copyright 2024 TikTok Pte. Ltd. All rights reserved.
 # Licensed under the Apache License Version 2.0 that can be found in the
 # LICENSE file in the root directory of this source tree.
 
