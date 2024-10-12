6a26c9f565072b0a2c2d3819d38dc5f351d99154
Jianliang Wang <125073156+jianliang00@users.noreply.github.com>
2024-10-12 03:08:22 +0000

    Revert "chore: update entity in license"
    This reverts commit 6653c7362dcabdf58a8d1ffac9c5cfa22939e80c.

--- core/__version__.py
+++ core/__version__.py
@@ -1,4 +1,4 @@
-# Copyright 2024 The TikTok, Inc. All rights reserved.
+# Copyright 2024 The habitat Authors. All rights reserved.
 # Licensed under the Apache License Version 2.0 that can be found in the
 # LICENSE file in the root directory of this source tree.
 
